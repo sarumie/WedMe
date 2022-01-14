8b439121b03355d9fd43f9e7ad35421b3effed1f
Sandy Raja Alamsyah <ikandarat123@gmail.com>
2022-01-14 15:46:39 +0700

    menu active

--- public/template/assets/js/custom.js
+++ public/template/assets/js/custom.js
@@ -6,3 +6,13 @@
  */
 
 "use strict";
+
+// Menu dropdown active
+var path = location.pathname.split('/');
+var url = location.origin + '/' + path[1];
+
+$('ul.sidebar-menu li a').each(function() {
+  if($(this).attr('href').indexOf(url) !== -1) {
+    $(this).parent().addClass('active').parent().parent('li').addClass('active');
+  }
+})
