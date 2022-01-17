bb100a8096def2e147b71a2811e8350460851080
Sandy Raja Alamsyah <ikandarat123@gmail.com>
2022-01-17 07:18:52 +0700

    link

--- app/Views/layout/default.php
+++ app/Views/layout/default.php
@@ -256,8 +256,8 @@ use App\Controllers\Auth;
           </ul>
 
           <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-              <i class="fas fa-rocket"></i> Upgrade to Pro
+            <a href="https://github.com/sarumie/WedMe" class="btn btn-primary btn-lg btn-block btn-icon-split">
+              <i class="fab fa-github"></i> Link Repository
             </a>
           </div>
         </aside>
